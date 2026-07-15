4749e7b5f0a19388a2a3e5bbb9fbedd0c0db1e4b
yashjain8448 <jainyash8448@gmail.com>
2026-07-15 17:16:26 +0530

    Testing fastapi

--- python-service/main.py
+++ python-service/main.py
@@ -1,10 +1,26 @@
+print("1")
 from dotenv import load_dotenv
+
+print("2")
 from utils.audio_processor import process_input
+
+print("3")
 from core.transcriber import transcribe_all
+
+print("4")
 from core.summarizer import summarize_transcript, generate_title
+
+print("5")
 from core.extractor import extract_action_items, extract_key_decisions, extract_questions
+
+print("6")
 from core.rag_engine import build_rag_chain
 
+print("7")
+
+load_dotenv()
+
+print("8")
 
 load_dotenv()
 
